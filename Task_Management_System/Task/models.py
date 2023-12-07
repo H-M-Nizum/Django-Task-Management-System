4ee06571e4f7b9f5a7d65e809d5366f1fd2a9f2a
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-07 21:03:08 +0600

    create model and relationship and admin panal

--- Task_Management_System/Task/models.py
+++ Task_Management_System/Task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class TaskModel(models.Model):
+    taskTitle = models.CharField(max_length=200)
+    taskDescription = models.TextField()
+    is_completed = models.BooleanField()
+    assign_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.taskTitle 
