cafacb17ff320ca8c7b530412514fa729f219c9f
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-08 00:55:37 +0600

    simple correction

--- Task_Management_System/Task/views.py
+++ Task_Management_System/Task/views.py
@@ -9,7 +9,7 @@ def add_task(request):
         task_form = forms.TaskForm(request.POST)
         if task_form.is_valid():
             task_form.save()
-            return redirect('add_task')
+            return redirect('show_task')
     else:
         task_form = forms.TaskForm()
     return render(request, 'add_tasks.html', {'form': task_form})
